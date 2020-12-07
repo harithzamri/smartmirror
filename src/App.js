9373d471f0d6b5289400052bd7db9c24a37f7957
Muhammad Harith Bin Zamri <harithtoikee96@gmail.com>
2020-12-07 10:07:39 +0800

    install react-router-dom and add fontawesomeicon cdn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import NavBarContainer from "./container/navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={NavBarContainer} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
