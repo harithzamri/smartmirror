5bbf5315cff3bffb140ef4e95c8c35fb46e00dfd
Muhammad Harith Bin Zamri <harithtoikee96@gmail.com>
2020-12-08 09:05:48 +0800

    add pages

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "../components/about";
+
+function AboutContainer() {
+  return (
+    <About>
+      <About.SmallText>
+        Your Factory automation solutions provider
+      </About.SmallText>
+    </About>
+  );
+}
+
+export default AboutContainer;
