e36ae32af7fa525fd0df05ea7fa84b578067f40a
Muhammad Harith Bin Zamri <harithtoikee96@gmail.com>
2020-12-10 08:59:58 +0800

    add global-styles for editing the whole css

---
+++ src/global-style.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    html, body {
+        font-family: Montserrat,Arial,sans-serif;
+        -wekit-font-smoothing: antialised;
+        -moz-osx-font-smoothing: grayscale;
+       
+    }
+`;
